adef7e9e696204e9bf46ec912c5485fefe582b15
diego.mancini <diemancini@gmail.com>
2024-06-05 21:37:28 +0100

    Refactoring milestone_2.py

--- milestone_2.py
+++ milestone_2.py
@@ -1,13 +1,24 @@
 import random
 
-word_list = ["Avocado", "Mango", "Banana", "Grape", "Orange"]
+
+def create_word_list():
+    return ["Avocado", "Mango", "Banana", "Grape", "Orange"]
+
+
+def choice_word(word_list):
+    return random.choice(word_list)
+
+
+def input_letter():
+    guess = input("Enter a single letter: ")
+    if len(guess) == 1 and str.isalpha(guess):
+        print(f"Good guess!")
+    else:
+        print(f"Oops! That is not a valid input.")
+
+
+word_list = create_word_list
 print(word_list)
-
-word = random.choice(word_list)
+word = choice_word(word_list)
 print(word)
-
-guess = input("Enter a single letter: ")
-if len(guess) == 1 and str.isalpha(guess):
-    print(f"Good guess!")
-else:
-    print(f"Oops! That is not a valid input.")
+input_letter()
